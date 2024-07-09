6923df50d3473d66cb80bcc8a0b2cdfcf5ce6aa6
marcelonapolioni <napolioni@outlook.com>
2024-07-08 21:23:29 -0300

    Criação de uma classedo pudantic que exclui o campo password

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,20 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from fast_zero.schemas import Message, UserSchema, UserPublic
 
 app = FastAPI()
 
+database = []
 
-@app.get('/', status_code=200)
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.post('/users/', response_model=UserPublic)
+def create_user(user: UserSchema):
+    return user
+
+
