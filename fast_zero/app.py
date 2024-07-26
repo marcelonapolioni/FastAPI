0bcbd671307c142596c7d1b2d5234e2bb264ec99
marcelonapolioni <napolioni@outlook.com>
2024-07-26 20:19:20 -0300

    [ADD] Criação dos testes

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,5 +1,4 @@
 from http import HTTPStatus
-import pytest
 
 from fastapi import FastAPI
 
@@ -10,15 +9,19 @@ app = FastAPI()
 database = []
 
 
-@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {"message": "Olá Mundo!"}
+    return {'message': 'Olá Mundo!'}
 
 
-@app.post("/users/", response_model=UserPublic)
+@app.post('/users/', response_model=UserPublic)
 def create_user(user: UserSchema):
     user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
 
     database.append(user_with_id)
 
     return user_with_id
+
+@app.get('/users/')
+def read_users():
+    return {'users': database}
